b84c1f5a5360cd3f052bc1a97fb5e5ee76db4676
tknr <geishagirlskoume@gmail.com>
2020-10-03 12:00:30 +0900

    2020年 10月  3日 土曜日 12:00:30 JST

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+mkdir -p ~/bin
+ln -s `pwd`/vro_split ~/bin/
+
+export_count=`cat ~/.bash_profile | grep 'export PATH="$HOME/bin:$PATH"' | wc -l`
+echo $export_count
+
+if [ $export_count -eq 0 ]; then
+	echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bash_profile
+fi
+echo "installed."
+echo "execute to complete installation:"
+echo "source ~/.bash_profile"
